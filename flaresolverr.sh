56fb6618cf07189da9644ee21dbf37c2d50910d7
quizzical86 <75372713+quizzical86@users.noreply.github.com>
2025-04-01 21:25:24 +0200

    Update flaresolverr.sh

--- flaresolverr.sh
+++ flaresolverr.sh
@@ -1,3 +1,24 @@
+#!/usr/bin/env bash
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
+# Copyright (c) 2021-2025 tteck
+# Author: tteck (tteckster) | Co-Author: remz1337
+# License: MIT | https://github.com/community-scripts/ProxmoxVE/raw/main/LICENSE
+# Source: https://github.com/FlareSolverr/FlareSolverr
+
+APP="FlareSolverr"
+var_tags="proxy"
+var_cpu="2"
+var_ram="2048"
+var_disk="4"
+var_os="debian"
+var_version="12"
+var_unprivileged="1"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
 function update_script() {
   header_info
   check_container_storage
@@ -83,3 +104,17 @@ function update_script() {
   # --- MODIFICATION END ---
   exit 0 # Indicate successful completion
 }
+
+start
+build_container
+description
+
+# --- NOTE ---
+# The changes above ONLY affect the 'update_script' function.
+# The initial installation via 'build_container' might install a different version.
+# This script will now forcefully install v3.3.21 when run again on the container.
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:8191${CL}"
